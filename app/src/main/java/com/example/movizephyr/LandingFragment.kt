f1a4d4645df877cb813f06072ea71f1ba8d5942b
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-18 16:48:42 +0545

    added auto navigate to home fragment after x second

--- app/src/main/java/com/example/movizephyr/LandingFragment.kt
+++ app/src/main/java/com/example/movizephyr/LandingFragment.kt
@@ -2,32 +2,63 @@ package com.example.movizephyr
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.view.WindowManager
+import androidx.navigation.fragment.findNavController
 import com.example.movizephyr.databinding.FragmentLandingBinding
 
 class LandingFragment : Fragment() {
 
     private lateinit var binding: FragmentLandingBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var delayedNavigation: Runnable? = null
+    private val handler: Handler by lazy { Handler() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLandingBinding.inflate(inflater,container, false)
+        binding = FragmentLandingBinding.inflate(inflater, container, false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // For API 28 and above, use postDelayed directly on the View
+            delayedNavigation = Runnable {
+                navigateToHomeFragment()
+            }
+            binding.root.postDelayed(delayedNavigation!!, 3000)
+        } else {
+            // For API versions less than 28, use the Handler
+            delayedNavigation = Runnable {
+                navigateToHomeFragment()
+            }
+            handler.postDelayed(delayedNavigation!!, 3000)
+        }
+
+        binding.btnToHome.setOnClickListener {
+            navigateToHomeFragment()
+        }
 
         return binding.root
     }
 
+    private fun navigateToHomeFragment() {
+        findNavController().navigate(
+            R.id.action_landingFragment_to_homeFragment
+        )
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        delayedNavigation?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                binding.root.removeCallbacks(it)
+            } else {
+                handler.removeCallbacks(it)
+            }
+        }
+    }
 
 }
 
