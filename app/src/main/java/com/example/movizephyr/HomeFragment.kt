9276050ef2575f160594ec734d28cc012a17f8dc
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-19 19:23:03 +0545

    added live data code as backend for home_fragment

--- app/src/main/java/com/example/movizephyr/HomeFragment.kt
+++ app/src/main/java/com/example/movizephyr/HomeFragment.kt
@@ -10,22 +10,31 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.movizephyr.adaptor.NowPlayingRecyclerViewAdaptor
-import com.example.movizephyr.adaptor.UpcomingRecyclerViewAdaptor
+import com.example.movizephyr.adaptor.movie.NowPlayingRecyclerViewAdaptor
+import com.example.movizephyr.adaptor.movie.PopularRecyclerViewAdaptor
+import com.example.movizephyr.adaptor.movie.TopRatedRecyclerViewAdaptor
+import com.example.movizephyr.adaptor.movie.UpcomingRecyclerViewAdaptor
 import com.example.movizephyr.databinding.FragmentHomeBinding
 import com.example.movizephyr.endpoints.movies.NowPlayingMovies
+import com.example.movizephyr.endpoints.movies.PopularMovies
+import com.example.movizephyr.endpoints.movies.TopRatedMovies
 import com.example.movizephyr.endpoints.movies.UpcomingMovies
 import com.example.movizephyr.modal.RetrofitInstance
 import com.example.movizephyr.modal.movies.nowplaying.NowPlaying
+import com.example.movizephyr.modal.movies.popular.Popular
+import com.example.movizephyr.modal.movies.toprated.TopRated
 import com.example.movizephyr.modal.movies.upcoming.UpComing
 import okhttp3.internal.toImmutableList
 import retrofit2.Response
+import retrofit2.create
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var retUpComing: UpcomingMovies
     private lateinit var retNowPlaying: NowPlayingMovies
+    private lateinit var retPopular: PopularMovies
+    private lateinit var retTopRated: TopRatedMovies
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +47,13 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         retUpComing = RetrofitInstance.getRetrofitInstance().create(UpcomingMovies::class.java)
         retNowPlaying = RetrofitInstance.getRetrofitInstance().create(NowPlayingMovies::class.java)
+        retPopular = RetrofitInstance.getRetrofitInstance().create(PopularMovies::class.java)
+        retTopRated = RetrofitInstance.getRetrofitInstance().create(TopRatedMovies::class.java)
 
         upcomingMovies()
         nowplayingMovies()
+        popularMovies()
+        topratedMovies()
 
         return binding.root
     }
@@ -85,6 +98,50 @@ class HomeFragment : Fragment() {
             if (nowplayingMoviesList != null) {
                 recyclerView.adapter =
                     NowPlayingRecyclerViewAdaptor(nowplayingMoviesList, context!!)
+            }
+        })
+    }
+
+    private fun popularMovies() {
+        val recyclerView = binding.popularRecyclerview
+        recyclerView.setBackgroundColor(Color.TRANSPARENT)
+        recyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        val responseLiveData: LiveData<Response<Popular>> = liveData {
+            val response =
+                retPopular.getPopularMovies("Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OTdkNGZlOTFlZGViZmJlODNiMzAzYjdkZTA3ODRiOSIsInN1YiI6IjY0YjNjOTlkMjNkMjc4MDBjOTNjNDdlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t4r-V0rOgS0n2DhOMd-Gd8E61jefu_fC-0FAjozOvaw")
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val popularList = it.body()?.results?.toImmutableList()
+            if (popularList!= null) {
+                recyclerView.adapter =
+                    PopularRecyclerViewAdaptor(popularList, context!!)
+            }
+        })
+    }
+
+    private fun topratedMovies() {
+        val recyclerView = binding.topratedRecyclerview
+        recyclerView.setBackgroundColor(Color.TRANSPARENT)
+        recyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        val responseLiveData: LiveData<Response<TopRated>> = liveData {
+            val response =
+                retTopRated.getTopRatedMovies("Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OTdkNGZlOTFlZGViZmJlODNiMzAzYjdkZTA3ODRiOSIsInN1YiI6IjY0YjNjOTlkMjNkMjc4MDBjOTNjNDdlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t4r-V0rOgS0n2DhOMd-Gd8E61jefu_fC-0FAjozOvaw")
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val popularList = it.body()?.results?.toImmutableList()
+            if (popularList!= null) {
+                recyclerView.adapter =
+                    TopRatedRecyclerViewAdaptor(popularList, context!!)
             }
         })
     }
