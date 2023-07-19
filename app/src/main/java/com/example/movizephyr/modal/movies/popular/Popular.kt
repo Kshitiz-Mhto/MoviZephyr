818f0083517f3944f170f03e985aa2d720c3d49a
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-19 19:21:15 +0545

    added models for top rated and popular movies

---
+++ app/src/main/java/com/example/movizephyr/modal/movies/popular/Popular.kt
@@ -0,0 +1,5 @@
+package com.example.movizephyr.modal.movies.popular
+
+data class Popular(
+    val results: List<Result>
+)
---
+++ app/src/main/java/com/example/movizephyr/modal/movies/toprated/TopRated.kt
@@ -0,0 +1,5 @@
+package com.example.movizephyr.modal.movies.toprated
+
+data class TopRated(
+    val results: List<Result>
+)
