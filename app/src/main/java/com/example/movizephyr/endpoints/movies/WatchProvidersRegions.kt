823d1a896f15fe83f7c9d826a997349b4c64acfb
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-09 18:48:13 +0545

    renamed the modal class for regions

--- app/src/main/java/com/example/movizephyr/endpoints/movies/WatchProvidersRegions.kt
+++ app/src/main/java/com/example/movizephyr/endpoints/movies/WatchProvidersRegions.kt
@@ -1,6 +1,6 @@
 package com.example.movizephyr.endpoints.movies
 
-import com.example.movizephyr.modal.movies.watchproviders.WatchProviders
+import com.example.movizephyr.modal.movies.watchproviders.WatchProvidersRegionsList
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -8,6 +8,6 @@ import retrofit2.http.Header
 interface WatchProvidersRegions {
 
     @GET("/3/watch/providers/regions")
-    suspend fun getWatchProviderRegion(@Header("Authorization") authorization: String):Response<WatchProviders>
+    suspend fun getWatchProviderRegion(@Header("Authorization") authorization: String):Response<WatchProvidersRegionsList>
 
 }
